e035a7703ec11f34ba54b177be90ce7f24ce07b2
Your Name <you@example.com>
2015-03-27 16:21:51 -0300

    3º Correção M7Ex1

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -1,141 +1,127 @@
 <?php
-
-
 return array(
 		'router' => array(
 				'routes' => array(
-						
-						'market'=>array(
-							'type'=>'literal',
-								'options'=>array(
-										'route'=> '/market1/',
-										'defaults'=> array(
-												'controller'=>'market-index-controller',
-												'action'=>'index'
-										),
-								
-								),
-								
-								'may_terminate'=> true,
-								'child_routes'=>array(
-										'slash' => array(
-												'type'    => 'Literal',
-												'options' => array(
-														'route'    => 'post1/',
-														'defaults' => array(
-																'controller'    => 'market-post-controller',
-																'action'        => 'index',
-														),
-												),
-												
-												),
-										
-										),
-								'may_terminate' => true,
-								'child_routes' => array(
-										'slash' => array(
-												'type'    => 'Literal',
-												'options' => array(
-														'route'    => 'view1/',
-														'defaults' => array(
-																'controller'    => 'market-view-controller',
-																'action'        => 'index',
-														),
-												),
-													
-										),
-										
-										
-								),
-								
-								
-								'market-view'=>array(
-										'type'=>'literal',
-										'options'=>array(
-												'route'=>'/market/view',
-												'defaults'=>array(
-														'controller'=>'market-view-controller',
-														'action'=>'index',
-															
-												),
-								
-										),
-										
-										
-										
-										'may_terminate' => true,
-											
-										'child_routes' => array(
-												#rota filha
-												'index' => array(
-														'type'    => 'Segment',
-														'options' => array(
-																'route'    => '/main[/:category]',
-																'defaults' => array(
-																		'action'=>'index'
-																),
-														),
-												),
-												
-												
-												)
-										
-										)
-											
-								),
-						'market-view'=>array(
-								
+						'market-view'=> array(
 								'type'=>'literal',
 								'options'=>array(
 										'route'=>'/market/view',
 										'defaults'=>array(
 												'controller'=>'market-view-controller',
 												'action'=>'index',
-													
-										),
-								
+									),
 								),
-								
+
 								'may_terminate' => true,
-									
 								'child_routes' => array(
 										#rota filha
 										'index' => array(
 												'type'    => 'Segment',
 												'options' => array(
-														'route'    => '/main[/:category]',
+														'route'    => '/main[/:category][/]',
 														'defaults' => array(
+																'controller'=>'market-view-controller',
 																'action'=>'index'
-														),
 												),
+											),
 										),
-										
+
 										'item'=>array(
 												'type'=>'Segment',
-												'options'=>array(			                                      'route'=>'/item[/:itemId]',
+												'options'=>array(
+														'route'=>'/item[/:itemId]',
 														'defaults'=>array(
 																'action'=>'item',
 														),
 														'constraints'=> array(
 																'itemId'=>'[0-9]*'
-																	
-														)
-												)
-												
-												)
+										              )
+									              )
+								             )
+							             ),
+						             ),#final view-market
+
+						'market'=>array(
+								'type'=>'literal',
+								'options'=>array(
+										'route'=> '/market',
+										'defaults'=> array(
+												'controller'=>'market-index-controller',
+												'action'=>'index'
+								   ),
+								),
+
+								'may_terminate'=> true,
+								'child_routes'=>array(
+										'slash' => array(
+												'type'    => 'Literal',
+												'options' => array(
+														'route'    => '/',
+														'defaults' => array(
+																'controller'    => 'market-index-controller',
+																'action'        => 'index',
+								    		   ),
+										   ),
 										),
-								
-						)
-						
-						
-					
-				)
+
+										'post'=> array(
+												'type'=>'Segment',
+												'options'=>array(
+														'route'=>'/post',
+														'defaults'=>array(
+																'controller'=>'market-post-controller',
+																'action'=>'index',
+												    ),
+												),
+
+												'may_terminate'=> true,
+												'child_routes'=>array(
+														'slash' => array(
+																'type'    => 'Literal',
+																'options' => array(
+																		'route'    => '/',
+																		'defaults' => array(
+																				'controller'    => 'market-post-controller',
+																				'action'        => 'index',
+														      ),
+													       ),
+												        ),
+											          ),
+										            ),
+
+										'view'=>array(
+												'type'=>'Segment',
+												'options'=>array(
+														'route'=>'/view',
+														'defaults'=>array(
+																'controller'=>'market-view-controller',
+																'action'=>'index',
+													),
+												),
+
+							'may_terminate'=> true,
+								'child_routes'=>array(
+								    'slash' => array(
+										'type'    => 'Literal',
+											'options' => array(
+											    'route'    => '/',
+												'defaults' => array(
+												'controller'    => 'market-view-controller',
+												'action'        => 'index',
+								 ),
+							   ),
+							),
+						  ),
+					   )
+				   ),
+			    )#final market
+	     	)
 		),
 
 		'controllers' => array(
 				'invokables' => array(
-				'market-index-controller' => 'Market\Controller\IndexController',
-				'market-view-controller'=> 'Market\Controller\ViewController'
+						'market-index-controller' => 'Market\Controller\IndexController',
+						'market-view-controller'=> 'Market\Controller\ViewController'
 				),
 				'factories'=>array(
 						#quando chamar o market-p-c, a factory será executada
