854ce57be1d894a0b85d929e3fa05709b3f94cc9
Your Name <you@example.com>
2015-04-08 15:58:12 -0300

    Correção M8ex1

--- module/Application/src/Application/Helper/LeftLinks.php
+++ module/Application/src/Application/Helper/LeftLinks.php
@@ -7,13 +7,15 @@ use Zend\View\Helper\AbstractHelper;
 class LeftLinks extends AbstractHelper {
 
 	public function __invoke(array $values, $urlPrefix){
-		$html = '<ul style="list-style-type: nome;">'. PHP_EOL;
+		$html = '<ul style="List-style-type: nome;">'. PHP_EOL;
+		
 		foreach ($values as $item){
 
-			$html.= sprint("<li><a href=\"%s/%s\">%s<a/></li>\n"
-					,$urlPrefix,$item,$item);
-
+			$html.= sprintf("<li><a href=\"%s/%s\">%s<a/></li>\n",
+					$urlPrefix,$item,$item);
+				
 		}
+		
 		$html.="</ul>".PHP_EOL;
 		return $html;
 
